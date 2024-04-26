773348516634c9811401d2915a5e271f1c0b9ccf
Santiconquest <161180110+Santiconquest@users.noreply.github.com>
2024-04-26 01:34:28 +0000

    Se entrega proyecto con boton funcionando

--- src/app.js
+++ src/app.js
@@ -2,10 +2,50 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //Numero del centro
+  const randomNumber = document.querySelector("#number");
+  let randomCard = () => {
+    let results = Math.floor(Math.random() * 12 + 2);
+    if (results == 11) results = "J";
+    if (results == 12) results = "Q";
+    if (results == 13) results = "K";
+    if (results == 14) results = "A";
+
+    return results;
+  };
+  randomNumber.innerHTML = `<h1>${randomCard()}</h1>`;
+
+  //Simbolos de las esquinas
+  const figuraSup = document.querySelector("#figura1");
+  let randomSimb = () => {
+    let result = Math.floor(Math.random() * 4 + 1);
+    if (result == 1) result = "♦";
+    if (result == 2) result = "♥";
+    if (result == 3) result = "♠";
+    if (result == 4) result = "♣";
+
+    return result;
+  };
+  //Repitiendo figura superior con inferior
+  const simboloFinal = randomSimb();
+  figuraSup.innerHTML = `<h1>${simboloFinal}</h1>`;
+  const figuraInf = document.querySelector("#figura2");
+  figuraInf.innerHTML = `<h1>${simboloFinal}</h1>`;
+
+  //Agregando color
+  if (simboloFinal == "♦" || simboloFinal == "♥") {
+    figuraSup.style.color = "red";
+    figuraInf.style.color = "red";
+  }
 };
+
+//Agregando boton
+const boton = document.createElement("button");
+boton.innerText = "Actualizar carta";
+document.body.appendChild(boton);
+
+boton.addEventListener("click", event => {
+  location.reload();
+});
